273be0e4549989269d92d5d2fa44335fe3774b57
Timothée Haudebourg <timothee.haudebourg@irisa.fr>
2020-03-24 18:07:54 +0100

    Add some examples.

---
+++ examples/sized.rs
@@ -0,0 +1,23 @@
+use std::io::{self, Read};
+use mown::Mown;
+
+fn response(input: &String) -> Mown<String> {
+	if input == "Hello\n" {
+		Mown::Owned("World!".to_string())
+	} else {
+		Mown::Borrowed(input)
+	}
+}
+
+fn main() -> io::Result<()> {
+	let mut buffer = String::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+
+    handle.read_to_string(&mut buffer)?;
+
+	let output = response(&buffer);
+	println!("{}", output);
+
+	Ok(())
+}
---
+++ examples/unsized.rs
@@ -0,0 +1,23 @@
+use std::io::{self, Read};
+use mown::Mown;
+
+fn response(input: &str) -> Mown<str> {
+	if input == "Hello\n" {
+		Mown::Owned("World!".to_string())
+	} else {
+		Mown::Borrowed(input)
+	}
+}
+
+fn main() -> io::Result<()> {
+	let mut buffer = String::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+
+    handle.read_to_string(&mut buffer)?;
+
+	let output = response(buffer.as_ref());
+	println!("{}", output);
+
+	Ok(())
+}
